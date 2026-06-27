ac73cfd84709b1fc279ae6d4ecf2c881b11df9c8
jpils <pilsj00@gmail.com>
2026-06-27 18:07:56 +0200

    removing undofile

--- config/nvim-plugins/telescope.nix
+++ config/nvim-plugins/telescope.nix
@@ -10,12 +10,7 @@
     ];
 
     lua = /* lua */ ''
-        local undodir = vim.fn.stdpath("data") .. "/undo"
-        if vim.fn.isdirectory(undodir) == 0 then
-            vim.fn.mkdir(undodir, "p")
-        end
-        vim.opt.undodir = undodir
-        vim.opt.undofile = true
+        vim.opt.undofile = false
         vim.opt.undolevels = 1000
 
         require("telescope").setup({
