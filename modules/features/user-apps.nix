23891ebb8ac53c9b332c5230b2d83d65ac05ee91
jpils <pilsj00@gmail.com>
2026-06-29 20:30:47 +0200

    changed scrolling keybindings for pi

--- modules/features/user-apps.nix
+++ modules/features/user-apps.nix
@@ -4,6 +4,7 @@
 		system.activationScripts.pi-agent-config.text = ''
 			install -d -m 700 -o jay -g users /home/jay/.pi/agent/extensions /home/jay/.pi/agent/themes
 			install -m 600 -o jay -g users ${../../config/pi/settings.json} /home/jay/.pi/agent/settings.json
+			install -m 600 -o jay -g users ${../../config/pi/keybindings.json} /home/jay/.pi/agent/keybindings.json
 			install -m 600 -o jay -g users ${../../config/pi/APPEND_SYSTEM.md} /home/jay/.pi/agent/APPEND_SYSTEM.md
 			install -m 600 -o jay -g users ${../../config/pi/extensions/confirm-file-mutations.ts} /home/jay/.pi/agent/extensions/confirm-file-mutations.ts
 			install -m 600 -o jay -g users ${../../config/pi/extensions/modal-editor.ts} /home/jay/.pi/agent/extensions/modal-editor.ts
