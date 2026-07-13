e1a6905aed2d6d95f7c90e592fc42562a5fea563
jpils <pilsj00@gmail.com>
2026-07-13 12:31:55 +0200

    polishing tmux resurrect

--- modules/features/tmux.nix
+++ modules/features/tmux.nix
@@ -22,9 +22,119 @@
 
 			[ -d "$dir" ] || exit 0
 
-			ls -1t "$dir"/tmux_resurrect_*.txt 2>/dev/null \
-				| tail -n +$((keep + 1)) \
+			last_target=""
+			if [ -L "$dir/last" ]; then
+				last_target="$(readlink -f "$dir/last")"
+			fi
+
+			keep_others="$keep"
+			if [ -n "$last_target" ] && [ -e "$last_target" ]; then
+				keep_others=$((keep - 1))
+			fi
+
+			find "$dir" -maxdepth 1 -type f -name 'tmux_resurrect_*.txt' -printf '%T@ %p\n' \
+				| sort -rn \
+				| awk -v keep_others="$keep_others" -v last_target="$last_target" '
+					$2 == last_target { next }
+					++count > keep_others { print $2 }
+				' \
 				| xargs -r rm -f --
+		'';
+
+		rewriteTmuxResurrectCommands = pkgs.writeShellScriptBin "rewrite-tmux-resurrect-commands" ''
+			set -euo pipefail
+
+			file="''${1:-}"
+			if [ -z "$file" ]; then
+				file="$HOME/.local/state/tmux/resurrect/last"
+			fi
+			[ -n "$file" ] && [ -f "$file" ] || exit 0
+			file="$(readlink -f "$file")"
+
+			${pkgs.perl}/bin/perl - "$file" <<'PERL'
+use strict;
+use warnings;
+use File::Temp qw(tempfile);
+use File::Basename qw(dirname);
+
+my $path = shift @ARGV;
+my %editor_windows;
+my %shell_windows;
+my %shell_dirs;
+my @lines;
+
+sub clean_dir {
+	my ($dir) = @_;
+	return undef unless defined $dir;
+	return undef unless $dir =~ /^:(?:\/|~)/;
+	$dir =~ s/^://;
+	$dir =~ s/^~/$ENV{HOME}/;
+	$dir =~ s/\\ / /g;
+	return $dir;
+}
+
+sub pane_dir {
+	my (@fields) = @_;
+	for my $idx (7, 6) {
+		my $dir = clean_dir($fields[$idx]);
+		return $dir if defined $dir;
+	}
+	return undef;
+}
+
+sub shell_quote {
+	my ($s) = @_;
+	$s =~ s/'/'"'"'/g;
+	return "'$s'";
+}
+
+open(my $in, '<', $path) or exit 0;
+while (my $line = <$in>) {
+	chomp $line;
+	push @lines, $line;
+
+	my @fields = split /\t/, $line, -1;
+	if (@fields >= 4 && $fields[0] eq 'window') {
+		my $key = "$fields[1]:$fields[2]";
+		$editor_windows{$key} = 1 if $fields[3] eq ':editor';
+		$shell_windows{$key} = $fields[1] if $fields[3] eq ':shell';
+	}
+}
+close $in;
+
+for my $line (@lines) {
+	my @fields = split /\t/, $line, -1;
+	next unless @fields >= 11 && $fields[0] eq 'pane';
+	next unless $shell_windows{"$fields[1]:$fields[2]"};
+	my $dir = pane_dir(@fields) or next;
+	my $session = $fields[1];
+	$shell_dirs{$session} = $dir if !exists $shell_dirs{$session} || $fields[8] eq '1';
+}
+
+my @out;
+for my $line (@lines) {
+	my @fields = split /\t/, $line, -1;
+	if (@fields >= 11 && $fields[0] eq 'pane' && $editor_windows{"$fields[1]:$fields[2]"}) {
+		my $dir = $shell_dirs{$fields[1]} // pane_dir(@fields) // '.';
+		my $quoted_dir = shell_quote($dir);
+		if (defined $fields[7] && $fields[7] =~ /^:(?:\/|~)/) {
+			$fields[7] = ":$dir";
+			splice @fields, 8, 0, '1' if !defined $fields[8] || $fields[8] !~ /^[01]$/;
+		} else {
+			splice @fields, 7, 0, ":$dir";
+		}
+		$fields[10] = ":cd $quoted_dir && direnv exec . yazi";
+		$#fields = 10;
+		$line = join "\t", @fields;
+	}
+	push @out, $line;
+}
+
+my ($fh, $tmp) = tempfile('tmux-resurrect-XXXXXX', DIR => dirname($path));
+print $fh join("\n", @out), "\n";
+close $fh;
+rename $tmp, $path or die "rename $tmp -> $path: $!";
+PERL
 		'';
 
 		mkTmux = { shellPath }: inputs.wrapper-modules.wrappers.tmux.wrap {
@@ -67,7 +177,9 @@
 				set -g @resurrect-dir '$HOME/.local/state/tmux/resurrect'
 				set -g @continuum-restore 'off'
 				set -g @continuum-save-interval '5'
-				set -g @resurrect-processes '"~pi -c->pi -c" "~bacon->direnv exec . bacon" "~yazi->direnv exec . yazi" "~nvim->NVIM_RESTORE_SESSION=1 nvim"'
+				set -g @resurrect-processes '"~pi -c->pi -c" "~bacon->direnv exec . bacon" "~yazi->direnv exec . yazi"'
+				set -g @resurrect-hook-post-save-layout '${rewriteTmuxResurrectCommands}/bin/rewrite-tmux-resurrect-commands'
+				set -g @resurrect-hook-pre-restore-all '${rewriteTmuxResurrectCommands}/bin/rewrite-tmux-resurrect-commands'
 				set -g @resurrect-hook-post-save-all '${cleanupTmuxResurrect}/bin/cleanup-tmux-resurrect'
 
 # Plugin Configs
